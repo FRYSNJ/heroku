89b62e7f49bf0888f4fef1606e9dcbcdcbf5026f
FRYSNJ <furuyas02@gmail.com>
2021-03-16 21:33:45 +0900

    再再編集

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php
-
-echo "Hello, Heroku!!!";
-echo "Welcom! Xlabo member";
-
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	</head>
+	</body>
+		<?php
+			echo "Hello, Heroku!!!<br />";
+			echo "Welcom! Xlabo member";
+		?>
+	</body>
+</html>
